438db88ac3fc787de1953f421bf4ebb2328244d2
Nikita Glukhi <nikita.glukhi@valor-software.com>
2019-05-17 17:15:31 +0300

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -50,4 +50,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Server started successfully'));
+app.listen(process.env.PORT, () => console.log('Server started successfully'));
